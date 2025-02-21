c245cc239afb275419fb9d0f08f128a7aae5aed1
Stef Konings <2302771konings@zuyd.nl>
2025-02-21 11:26:40 +0100

    Database toegevoegd

--- FotoApp/App.xaml.cs
+++ FotoApp/App.xaml.cs
@@ -1,10 +1,18 @@
-﻿namespace FotoApp
+﻿using FotoApp.MVVM.Data;
+namespace FotoApp
 {
     public partial class App : Application
     {
+        public static Constants Database { get; private set; }
         public App()
         {
             InitializeComponent();
+            InitializeDatabase();
+        }
+        private void InitializeDatabase()
+        {
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "FotoApp.db");
+            Database = new Constants(dbPath);
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
--- FotoApp/MVVM/Model/User.cs
+++ FotoApp/MVVM/Model/User.cs
@@ -3,19 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace FotoApp.MVVM.Model
 {
     public class User
     {
+        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
+        [NotNull]
         public string Username { get; set; }
+        [NotNull]
         public string Email { get; set; }
-        public string PasswordHash { get; set; }
+        [NotNull]
+        public string Password { get; set; }
+        [NotNull]
         public int Points { get; set; } = 5;
+        [NotNull]
         public bool IsSuperMember { get; set; } = false;
+        [NotNull]
         public UserRole Role { get; set; } = UserRole.Member;
+        [Ignore]
         public ICollection<Photo> Photos { get; set; }
+        [Ignore]
         public ICollection<Comment> Comments { get; set; }
     }
     public enum UserRole
